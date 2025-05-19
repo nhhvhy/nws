6279db1c857c02718d722111f667527ddb371124
nhhvhy <maxcostler@gmail.com>
2025-05-19 17:47:02 -0600

    Basic request implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let client = reqwest::Client::new();
+
+    let res = client.get("http://testphp.vulnweb.com")
+        .send()
+        .await?;
+
+    eprintln!("Headers: {:#?}\n", res.headers());
+    Ok(())
 }
