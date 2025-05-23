79ebac7a89258a34415d7ed7c2b2728e82fe8c0d
nhhvhy <maxcostler@gmail.com>
2025-05-22 20:42:24 -0600

    restructuring

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 use std::process::Command;
 use std::str;
 use std::error::Error;
+// use std::collections::HashMap;
 use reqwest::Error as ReqwestError;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    fetch_headers("http://testphp.vulnweb.com").await?;
+
+    let address = "http://scanme.nmap.org".to_string();
+
+    let _headers = fetch_headers(&address).await?;
+
+    let mut flags = Vec::new();
+    flags.push("-sV".to_string());
+
+
+    let nmap = nmap(flags, "scanme.nmap.org".to_string());
+    eprintln!("\n{:?}", nmap);
+
     Ok(())
 }
 
 
-fn nmap(flags: Vec<&str>, address: &str) -> Result<String, Box<dyn Error>> {
+fn nmap(flags: Vec<String>, address: String) -> Result<String, Box<dyn Error>> {
     let output = Command::new("nmap")
         .args(&flags)
         .args(&[address])
-        .output()?;
+        .output()
+        .expect("failed to execute nmap command");
 
     if !output.status.success() {
         return Err(format!("nmap failed: {}", String::from_utf8_lossy(&output.stderr)).into());
@@ -31,6 +45,10 @@ async fn fetch_headers(address: &str) -> Result<(), ReqwestError> {
         .send()
         .await?;
 
-    eprintln!("Headers: {:#?}\n", res.headers());
+    let headers = res.headers();
+    for (key, value) in headers.iter() {
+        eprintln!("{:?}: {:?}", key, value);
+    }
+
     Ok(())
 }
