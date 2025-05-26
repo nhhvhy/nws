34f998ecb82b37d0b25d7433bbc168ccafbef19e
nhhvhy <maxcostler@gmail.com>
2025-05-26 00:16:35 -0600

    Added CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,55 @@
 use std::process::Command;
 use std::str;
+use std::env;
 use std::error::Error;
 // use std::collections::HashMap;
 use reqwest::Error as ReqwestError;
 
+struct Params {
+    url: String,
+    fqdn: String,
+    flags: Vec<String>
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 
-    let address = "http://scanme.nmap.org".to_string();
+    let params: Params = collect_params();
 
-    let _headers = fetch_headers(&address).await?;
-
-    let mut flags = Vec::new();
-    flags.push("-sV".to_string());
-
-
-    let nmap = nmap(flags, "scanme.nmap.org".to_string());
+    let _headers = fetch_headers(&params.url).await?;
+    
+    let nmap = nmap(params.fqdn, params.flags);
     eprintln!("\n{:?}", nmap);
 
     Ok(())
 }
 
+fn collect_params() -> Params {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() < 3 {
+        return Params {
+            url: "http://scanme.nmap.org".to_string(),
+            fqdn: "scanme.nmap.org".to_string(),
+            flags: vec![]
+        };
+    }
 
-fn nmap(flags: Vec<String>, address: String) -> Result<String, Box<dyn Error>> {
+    let url = String::from(&args[1]);
+    let fqdn = String::from(&args[2]);
+    let flags = Vec::from(&args[3..]);
+    // dbg!(&url, &fqdn, &flags);
+
+    return Params {
+        url: url,
+        fqdn: fqdn,
+        flags: flags
+    }
+
+}
+
+// nmap doesn't like protocols, use fqdn instead of url
+fn nmap(address: String, flags: Vec<String>) -> Result<String, Box<dyn Error>> {
     let output = Command::new("nmap")
         .args(&flags)
         .args(&[address])
